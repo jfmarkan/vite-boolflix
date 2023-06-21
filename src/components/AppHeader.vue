f2b205e1c1f33ed9c3c148321af590cf51e1a46b
jfmarkan <jfmarkan@gmail.com>
2023-06-21 12:12:51 +0200

    Added serach bar and button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
-        <h1>HEADER</h1>
+        <input type="text">
+        <button>Search</button>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'AppHeader',
 }
